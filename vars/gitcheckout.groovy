413c73cb8e8416321eb8e010e281ca6fd08714a3
sudhakarbastawade2303 <sudhakarbastawade2303@gmail.com>
2022-11-18 23:47:47 +0530

    update vars

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,3 +1,3 @@
 def cal(checkout){
-checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/sudhakarbastawade2303/jenkinssharedlib.git']]])
+checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/sudhakarbastawade2303/spring-boot-war-example.git']]])
 }
